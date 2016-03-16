557fe2499789fada3209aeb7e2b6055fc022f9dc
benjimor <benji_moreau@hotmail.fr>
2016-03-16 17:46:29 +0100

    piano,score resizing; notename fixed

--- src/EZpianoview.cpp
+++ src/EZpianoview.cpp
@@ -1,9 +1,19 @@
 #include "../include/EZpianoview.hpp"
 
-EzPianoView::EzPianoView(QGraphicsScene* sc):QGraphicsView(sc){}
+EzPianoView::EzPianoView(QGraphicsScene* sc):QGraphicsView(sc){
+    setHorizontalScrollBarPolicy(Qt::ScrollBarAlwaysOff);
+    setVerticalScrollBarPolicy(Qt::ScrollBarAlwaysOff);
+   setRenderHints(QPainter::Antialiasing);
+}
 EzPianoView::~EzPianoView(){}
 
 void EzPianoView::mousePressEvent(QMouseEvent * mouseEvent){
 	//QMessageBox::information(new QWidget(),QString::number(mouseEvent->pos().x()),QString::number(mouseEvent->pos().y()));
 	QGraphicsView::mousePressEvent(mouseEvent);
+}
+
+void EzPianoView::resizeEvent(QResizeEvent *event)
+{
+    fitInView(scene()->sceneRect() ,Qt::KeepAspectRatio);
+    QGraphicsView::resizeEvent(event);
 }
