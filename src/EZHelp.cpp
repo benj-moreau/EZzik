2baad1a4d2745601316985bdc5975c4a44f52117
Jasone-Lenormand <buckethead@gmx.fr>
2016-03-20 18:22:55 +0100

    niceru

--- src/EZHelp.cpp
+++
@@ -1,9 +0,0 @@
-#include "../include/EZHelp.hpp"
-
-EzHelp::EzHelp():QWidget(){
-	QVBoxLayout *mainLayout = new QVBoxLayout();
-	QHBoxLayout *folder = new QHBoxLayout();
-	QHBoxLayout *reset = new QHBoxLayout();
-}
-
-EzHelp::~EzHelp(){}
